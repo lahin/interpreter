be062a9d3e0ba68f4cd8ad51962c525dbc6f88e5
Singh Nihal <getnihal.09@gmail.com>
2026-02-06 00:34:20 -0500

    Add support for multiple and some cleanup
    Add support for multiple and some cleanup

--- Calculator/calc1.py
+++ Calculator/calc1.py
@@ -37,6 +37,13 @@ class Interpreter(object):
     def error(self, str):
         raise Exception(str)
 
+    def get_integer(self):
+        text = self.text
+        start = self.pos
+        while self.pos < len(text) and text[self.pos].isdigit():
+            self.pos += 1
+        return int(text[start : self.pos])
+
     def get_next_token(self):
         """Lexical analyzer (also known as scanner or tokenizer).
 
@@ -47,48 +54,33 @@ class Interpreter(object):
 
         # Is `self.pos` index past the end of the `self.text`?
         # If so, then retrun `EOF` token because there is no more calculation/program left.
-        if self.pos > len(text) - 1:
+        if self.pos >= len(text):
             return Token(EOF, None)
 
         current_char = text[self.pos]
 
-        # If the current character is digit, return `INTEGER`
         if current_char.isdigit():
-            self.pos += 1
-            return Token(INTEGER, int(current_char))
+            return Token(INTEGER, self.get_integer())
 
-        # if the current character is `+`, return `PLUS`
         if current_char == "+":
             self.pos += 1
             return Token(PLUS, current_char)
 
-        self.error(
-            "Parsing failed, unknown symbol {symbol}".format(symbol=current_char)
-        )
+        self.error(f"Parsing failed, unknown symbol {current_char!r}")
 
     def expr(self):
         """expr -> INTEGER PLUS INTEGER"""
         left_operand = self.get_next_token()
         if left_operand.type != INTEGER:
-            self.error(
-                "Sytax Error: expected integer got {symbol}".format(
-                    symbol=left_operand.value
-                )
-            )
+            self.error(f"Sytax Error: expected integer got {left_operand!r}")
 
         operand = self.get_next_token()
         if operand.type != PLUS:
-            self.error(
-                "Sytax Error: expected '+' got {symbol}".format(symbol=operand.value)
-            )
+            self.error(f"Sytax Error: expected '+' got {operand!r}")
 
         right_operand = self.get_next_token()
         if right_operand.type != INTEGER:
-            self.error(
-                "Sytax Error: expected integer got {symbol}".format(
-                    symbol=right_operand.value
-                )
-            )
+            self.error(f"Sytax Error: expected integer got {right_operand!r}")
 
         return left_operand.value + right_operand.value
 
