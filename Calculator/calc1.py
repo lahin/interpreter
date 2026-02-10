613e9440e0cc0b43f3c794dd26217648fcddfe0f
Singh Nihal <getnihal.09@gmail.com>
2026-02-09 23:34:55 -0500

    Improve code design
    - Introduce Lexer
    - Change design to accomodate testing

--- Calculator/calc1.py
+++ Calculator/calc1.py
@@ -26,7 +26,7 @@ class Token(object):
     __repr__ = __str__
 
 
-class Interpreter(object):
+class Lexer(object):
     operators = {
         "+": operator.add,
         "-": operator.sub,
@@ -40,27 +40,34 @@ class Interpreter(object):
         # Index into the program/calculation
         self.pos = 0
 
-    def error(self, err_str):
+    @staticmethod
+    def error(err_str):
         raise Exception(err_str)
 
-    def get_integer(self):
-        text = self.text
-        start = self.pos
-        while self.pos < len(text) and text[self.pos].isdigit():
-            self.pos += 1
-        return int(text[start : self.pos])
+    @staticmethod
+    def get_integer(text, pos):
+        assert pos >= 0
+        start = pos
+        while pos < len(text) and text[pos].isdigit():
+            pos += 1
+        return (int(text[start:pos]), pos)
+
+    @staticmethod
+    def skip_whitespaces(text, pos):
+        assert pos >= 0
+        # Skip spaces and tabs
+        while pos < len(text) and text[pos].isspace():
+            pos += 1
+        return pos
 
     def get_next_token(self):
         """Lexical analyzer (also known as scanner or tokenizer).
-
         This method is responsible for breaking a sentence apart into tokens.
         One token at a time.
         """
-        text = self.text
+        self.pos = Lexer.skip_whitespaces(self.text, self.pos)
 
-        # Skip spaces and tabs
-        while self.pos < len(text) and text[self.pos].isspace():
-            self.pos += 1
+        text = self.text
 
         # Is `self.pos` index past the end of the `self.text`?
         # If so, then retrun `EOF` token because there is no more calculation/program left.
@@ -70,30 +77,43 @@ class Interpreter(object):
         current_char = text[self.pos]
 
         if current_char.isdigit():
-            return Token(INTEGER, self.get_integer())
+            integer_value, self.pos = Lexer.get_integer(self.text, self.pos)
+            return Token(INTEGER, integer_value)
 
-        op = Interpreter.operators.get(current_char)
+        op = Lexer.operators.get(current_char)
         if not op is None:
             self.pos += 1
             return Token(OPERATOR, op)
 
         self.error(f"Parsing failed, unknown symbol {current_char!r}")
 
+
+class Interpreter(object):
+    def __init__(self, text):
+        self.lexer = Lexer(text)
+
+    def error(self, err_str):
+        raise Exception(err_str)
+
     def expr(self):
         """expr -> INTEGER PLUS INTEGER"""
-        left_operand = self.get_next_token()
+        left_operand = self.lexer.get_next_token()
         if left_operand.type != INTEGER:
             self.error(f"Sytax Error: expected integer got {left_operand!r}")
 
-        op = self.get_next_token()
+        op = self.lexer.get_next_token()
         if op.type != OPERATOR:
             self.error(f"Sytax Error: expected operand got {op!r}")
 
-        right_operand = self.get_next_token()
+        right_operand = self.lexer.get_next_token()
         if right_operand.type != INTEGER:
             self.error(f"Sytax Error: expected integer got {right_operand!r}")
 
-        return op.value(left_operand.value, right_operand.value)
+        eof_token = self.lexer.get_next_token()
+        if eof_token.type == EOF:
+            return op.value(left_operand.value, right_operand.value)
+
+        self.error("Text detected instead of end of file")
 
 
 def main():
